4a354dc6be28affaeae2d86cd00cca2a735ca9a1
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-05 11:24:41 +0530

    05-07-26

---
+++ bit++.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x = 0;
+    int y;
+    cin >> y;
+    for(int i = 0; i<y; i++){
+        string a;
+        cin >> a;
+        if(a[1] == '+'){
+            x++;
+        }
+        else{
+            x--;
+        }
+    }
+
+    cout << x << endl;
+    return 0;
+    
+
+
+
+}
+
+//other way
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int x=0;
+//     for(int i=0;i<n;i++){
+//         string v;
+//         cin>>v;
+//         if(v=="X++" || v=="++X"){
+//             x++;
+//         }
+//         else {
+//             x--;
+//         }
+//     }
+//     cout<<x;
+
+//     return 0;
+// }
