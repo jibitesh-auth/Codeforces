9339cc2c54fb8dae759d7b9afd232ff7bbaf6f38
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-05 17:12:45 +0530

    05-07-26

---
+++ Chewbaсca and Number(514A)/A_Chewbacca_and_Number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long num;
+    cin >> num;
+    string a = to_string(num);
+    for(int i=0; i<a.size(); i++){
+        int b = a[i] - '0';
+        int c = 9 - b;
+        if(i == 0 && c == 0){
+            continue;
+        }
+        if(c < b){
+            a[i] = c + '0';
+
+        }
+
+    }
+
+    cout << a;
+    return 0;
+
+}
