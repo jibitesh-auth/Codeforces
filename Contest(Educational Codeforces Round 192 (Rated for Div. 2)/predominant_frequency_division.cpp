d294a640db941bf6e2832494ac5cd97a54e4f082
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-07-13 21:04:24 +0530

    13-07-26

--- Contest(Educational Codeforces Round 192 (Rated for Div. 2)/predominant_frequency_division.cpp
+++ Contest(Educational Codeforces Round 192 (Rated for Div. 2)/predominant_frequency_division.cpp
@@ -1,79 +1,181 @@
-#include<bits/stdc++.h>
+//O(n3)
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     int t;
+//     cin >> t;
+
+//     while (t--) {
+
+//         int n;
+//         cin >> n;
+
+//         vector<int> a(n);
+
+//         for (int i = 0; i < n; i++)
+//             cin >> a[i];
+
+//         bool ok = false;
+
+//         for (int left = 0; left <= n - 3 && !ok; left++) {
+
+//             int one = 0, two = 0, three = 0;
+
+//             // Count LEFT
+//             for (int i = 0; i <= left; i++) {
+//                 if (a[i] == 1) one++;
+//                 else if (a[i] == 2) two++;
+//                 else three++;
+//             }
+
+//             if (one < two + three)
+//                 continue;
+
+//             for (int mid = left + 1; mid <= n - 2 && !ok; mid++) {
+
+//                 one = two = three = 0;
+
+//                 // Count MIDDLE
+//                 for (int i = left + 1; i <= mid; i++) {
+//                     if (a[i] == 1) one++;
+//                     else if (a[i] == 2) two++;
+//                     else three++;
+//                 }
+
+//                 if (one + two >= three)
+//                     ok = true;
+//             }
+//         }
+
+//         cout << (ok ? "YES" : "NO") << '\n';
+//     }
+// }
+
+
+
+// //O(n2)
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     int t;
+//     cin >> t;
+
+//     while (t--) {
+
+//         int n;
+//         cin >> n;
+
+//         vector<int> a(n);
+
+//         vector<int> p1(n + 1, 0);
+//         vector<int> p2(n + 1, 0);
+//         vector<int> p3(n + 1, 0);
+
+//         for (int i = 0; i < n; i++) {
+
+//             cin >> a[i];
+
+//             p1[i + 1] = p1[i];
+//             p2[i + 1] = p2[i];
+//             p3[i + 1] = p3[i];
+
+//             if (a[i] == 1)
+//                 p1[i + 1]++;
+//             else if (a[i] == 2)
+//                 p2[i + 1]++;
+//             else
+//                 p3[i + 1]++;
+//         }
+
+//         bool ok = false;
+
+//         for (int left = 0; left <= n - 3 && !ok; left++) {
+
+//             int one = p1[left + 1];
+//             int two = p2[left + 1];
+//             int three = p3[left + 1];
+
+//             if (one < two + three)
+//                 continue;
+
+//             for (int mid = left + 1; mid <= n - 2 && !ok; mid++) {
+
+//                 one = p1[mid + 1] - p1[left + 1];
+//                 two = p2[mid + 1] - p2[left + 1];
+//                 three = p3[mid + 1] - p3[left + 1];
+
+//                 if (one + two >= three)
+//                     ok = true;
+//             }
+//         }
+
+//         cout << (ok ? "YES" : "NO") << '\n';
+//     }
+// }
+
+//O(n)
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    unsigned int t;
-    
-    int z = 0;
+const int INF = 1e9;
+
+void solve() {
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vector<int> pref1(n + 1, 0);
+    vector<int> pref2(n + 1, 0);
+
+    for (int i = 0; i < n; i++) {
+
+        if (a[i] == 1)
+            pref1[i + 1] = pref1[i] + 1;
+        else
+            pref1[i + 1] = pref1[i] - 1;
+
+        if (a[i] == 3)
+            pref2[i + 1] = pref2[i] - 1;
+        else
+            pref2[i + 1] = pref2[i] + 1;
+    }
+
+    int mn = INF;
+
+    for (int i = 1; i < n; i++) {
+
+        if (pref2[i] >= mn) {
+            cout << "YES\n";
+            return;
+        }
+
+        if (pref1[i] >= 0)
+            mn = min(mn, pref2[i]);
+    }
+
+    cout << "NO\n";
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
     cin >> t;
-    int c[t];
-    while(t--){
-        z++;
-        unsigned int n;
-        cin >> n;
-        if(n < 3){
-            cout<<"Wrong Input";
-            break;
-        }
-        int arr[n];
-        int count1= 0, count2 = 0, count3 = 0;
-        int left=-1, middle=-1;
-        bool a = false;
-        
-        
-        for(int i=0; i<n; i++){
-            cin >> arr[i];
-            if(arr[i] > 3 || arr[i] < 1){
-                cout << "Wrong Input";
-                return 0;
-            }
 
-            if(arr[i] == 1){
-                count1++;
-            }
-            else if(arr[i] == 2){
-                count2++;
-            }
-            else{
-                count3++;
-            }
-
-            if((count1 >= count2 + count3) && left == -1 && middle == -1){
-                left = i;
-                count1 =0,count2=0,count3=0;
-                
-            }
-            else if((count1 + count2 >= count3) && left != -1 && middle == -1){
-                middle = i;
-                count1 =0,count2=0,count3=0;
-            }
-            else if(((n-1) > middle ) && middle != -1 && left != -1){
-                a = true;
-
-            }
-            
-        }
-        if (a){
-            c[z-1] = 1;
-        }
-        else{
-            c[z-1] = 0;
-        }
-    
-        //left-> count1 >= count2 + count3
-        //middle-> count1 + count2 >= count3
-        //right-> non-empty
-
-    }
-    for(int i=0; i<z; i++){
-        if(c[i]){
-            cout<<"YES" << endl;
-        }
-        else{
-            cout<<"NO << endl";
-        }
-
-    }
-
-    return 0;
+    while (t--)
+        solve();
 }
