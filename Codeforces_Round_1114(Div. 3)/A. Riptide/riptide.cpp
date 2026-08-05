5bf131c5aad7de9e36202671d6cb9755f9699b10
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-08-05 17:52:35 +0530

    05-08-26

--- Codeforces_Round_1114(Div. 3)/A. Riptide/riptide.cpp
+++ Codeforces_Round_1114(Div. 3)/A. Riptide/riptide.cpp
@@ -1,52 +1,81 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve(int a,int b, int c){
+//     int rounds = 0;
+//     while(true){
+//         if(a == b || a == c || b == c){
+//             break;
+//         }
+//         else{
+//             rounds++;
+//             int m = max({a,b,c});
+//             int l = min({a,b,c});
+//             if(m == a){
+//                 a--;
+//             }
+//             else if(m == b){
+//                 b--;
+//             }
+//             else{
+//                 c--;
+//             }
+
+//             if(l == a){
+//                 a++;
+//             }
+//             else if(l == b){
+//                 b++;
+//             }
+//             else{
+//                 c++;
+//             }
+
+
+//         }
+//     }
+//     cout << rounds<< endl;
+// }
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     int n,a,b,c;
+//     cin >> n;
+//     for(int i = 0; i<n; i++){
+//         cin >>a >> b>>c;
+//         solve(a,b,c);
+
+//     }
+
+
+//     return 0;
+// }
+
+
+//------------------x----------------
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(int a,int b, int c){
-    int rounds = 0;
-    while(true){
-        if(a == b || a == c || b == c){
-            break;
-        }
-        else{
-            rounds++;
-            int m = max({a,b,c});
-            int l = min({a,b,c});
-            if(m == a){
-                a--;
-            }
-            else if(m == b){
-                b--;
-            }
-            else{
-                c--;
-            }
-
-            if(l == a){
-                a++;
-            }
-            else if(l == b){
-                b++;
-            }
-            else{
-                c++;
-            }
-
-
-        }
-    }
-    cout << rounds<< endl;
-}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n,a,b,c;
-    cin >> n;
-    for(int i = 0; i<n; i++){
-        cin >>a >> b>>c;
-        solve(a,b,c);
-
+    int t;
+    cin >> t;
+    while(t--){
+        vector<int> v(3);
+        for(auto x : v){
+            cin >> x;
+        }
+        sort(v.begin(), v.end());
+        
     }
+    //Hello
+
+    
 
 
     return 0;
