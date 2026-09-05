ee15ad1da371abcf85e2caf65770edbc45f6524f
jibitesh-auth <jibiteshkumarmishra8027@gmail.com>
2026-09-05 17:03:45 +0530

    05-09-26

--- Contest(Educational Codeforces Round 192 (Rated for Div. 2)/bigrams.cpp
+++ Contest(Educational Codeforces Round 192 (Rated for Div. 2)/bigrams.cpp
@@ -1,34 +1,48 @@
-#include<bits/stdc++.h>
+//*https://codeforces.com/contest/2242/problem/A
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    
     int t;
     cin >> t;
     while(t--){
-        int k;
-        cin >> k;
         bool ok = false;
-        int twice = 0;
-
-        for(int i=0; i<k; i++){
-            int x;
+        int n,x;
+        cin >> n;
+        int count = 0;
+        for(int i=0; i<n; i++){
             cin >> x;
-            if(x >= 3){
-                ok = true; 
-            }
             if(x == 2){
-                twice++;
+                count++;
+            }
+            if(x >= 3){
+                ok = true;
             }
         }
-        if(ok || twice >= 2){
-            cout << "YES\n";
+        if(ok || count > 1){
+            cout << "YES" << endl;
         }
         else{
-            cout << "NO\n";
+            cout << "NO" << endl;
         }
+
+
+        
     }
     return 0;
 }
- 
+
+//*T.C: O(t * k)
+//*S.C: O(1)
+//----------------X---------------
+//*LOGIC:
+/*
+Suppose we are having 1 2 3 which means a bb ccc
+To check for a bigram we need 2 conditions:-
+1. one of the no. must be >2 [e.g: 1 2 3 so ccc we can make cc cc[Bigram] ]
+2. Atleast 2 of the no should be == 2[e.g: 1 2 2, so here bb cc so we can make bcbc: Bigram]
+*/
--- Contest(Educational Codeforces Round 192 (Rated for Div. 2)/predominant_frequency_division.cpp
+++ Contest(Educational Codeforces Round 192 (Rated for Div. 2)/predominant_frequency_division.cpp
@@ -1,181 +1,99 @@
-//O(n3)
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-
-//     int t;
-//     cin >> t;
-
-//     while (t--) {
-
-//         int n;
-//         cin >> n;
-
-//         vector<int> a(n);
-
-//         for (int i = 0; i < n; i++)
-//             cin >> a[i];
-
-//         bool ok = false;
-
-//         for (int left = 0; left <= n - 3 && !ok; left++) {
-
-//             int one = 0, two = 0, three = 0;
-
-//             // Count LEFT
-//             for (int i = 0; i <= left; i++) {
-//                 if (a[i] == 1) one++;
-//                 else if (a[i] == 2) two++;
-//                 else three++;
-//             }
-
-//             if (one < two + three)
-//                 continue;
-
-//             for (int mid = left + 1; mid <= n - 2 && !ok; mid++) {
-
-//                 one = two = three = 0;
-
-//                 // Count MIDDLE
-//                 for (int i = left + 1; i <= mid; i++) {
-//                     if (a[i] == 1) one++;
-//                     else if (a[i] == 2) two++;
-//                     else three++;
-//                 }
-
-//                 if (one + two >= three)
-//                     ok = true;
-//             }
-//         }
-
-//         cout << (ok ? "YES" : "NO") << '\n';
-//     }
-// }
-
-
-
-// O(n2)
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-
-//     int t;
-//     cin >> t;
-
-//     while (t--) {
-
-//         int n;
-//         cin >> n;
-
-//         vector<int> a(n);
-
-//         vector<int> p1(n + 1, 0);
-//         vector<int> p2(n + 1, 0);
-//         vector<int> p3(n + 1, 0);
-
-//         for (int i = 0; i < n; i++) {
-
-//             cin >> a[i];
-
-//             p1[i + 1] = p1[i];
-//             p2[i + 1] = p2[i];
-//             p3[i + 1] = p3[i];
-
-//             if (a[i] == 1)
-//                 p1[i + 1]++;
-//             else if (a[i] == 2)
-//                 p2[i + 1]++;
-//             else
-//                 p3[i + 1]++;
-//         }
-
-//         bool ok = false;
-
-//         for (int left = 0; left <= n - 3 && !ok; left++) {
-
-//             int one = p1[left + 1];
-//             int two = p2[left + 1];
-//             int three = p3[left + 1];
-
-//             if (one < two + three)
-//                 continue;
-
-//             for (int mid = left + 1; mid <= n - 2 && !ok; mid++) {
-
-//                 one = p1[mid + 1] - p1[left + 1];
-//                 two = p2[mid + 1] - p2[left + 1];
-//                 three = p3[mid + 1] - p3[left + 1];
-
-//                 if (one + two >= three)
-//                     ok = true;
-//             }
-//         }
-
-//         cout << (ok ? "YES" : "NO") << '\n';
-//     }
-// }
-
-// O(n)
+//*https://codeforces.com/contest/2242/problem/B
 
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e9;
+const int INF = 1'000'000'007;
 
-void solve() {
-
+void solve(){
     int n;
     cin >> n;
-
     vector<int> a(n);
-
-    for (int i = 0; i < n; i++)
+    for(int i=0; i<n; i++){
         cin >> a[i];
+    }
 
-    vector<int> pref1(n + 1, 0);
-    vector<int> pref2(n + 1, 0);
+    vector<int> pref1(n+1);
+    vector<int> pref2(n+1);
+    for(int i=0; i<n; i++){
+        pref1[i+1] = pref1[i] + (a[i] == 1 ? 1 : -1);
 
-    for (int i = 0; i < n; i++) {
+        pref2[i+1] = pref2[i] + (a[i] == 3 ? -1:1);
 
-        if (a[i] == 1)
-            pref1[i + 1] = pref1[i] + 1;
-        else
-            pref1[i + 1] = pref1[i] - 1;
-
-        if (a[i] == 3)
-            pref2[i + 1] = pref2[i] - 1;
-        else
-            pref2[i + 1] = pref2[i] + 1;
     }
 
     int mn = INF;
-
-    for (int i = 1; i < n; i++) {
-
-        if (pref2[i] >= mn) {
-            cout << "YES\n";
+    for(int i=1;i<n; i++){
+        if(pref2[i] - mn >=0){
+            cout << "YES"<<endl;
             return;
         }
-
-        if (pref1[i] >= 0)
-            mn = min(mn, pref2[i]);
+        if(pref1[i] >= 0){
+            mn = min(mn,pref2[i]);
+        }
     }
 
     cout << "NO\n";
 }
 
-int main() {
 
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
     cin >> t;
-
-    while (t--)
+    while(t--){
         solve();
+    }
+    return 0;
 }
+
+
+
+//*T.C: O(N)
+//*S.C: O(N)
+//-----------------------------x------------------------------
+//*Logic:-
+/*we have to break the array into 3 parts such that:-
+1. 1st part------> 1 >= 2 & 3
+2. 2nd Part------> 1 & 2 >= 3
+3. 3rd Part------> Non Empty
+
+We will take x & y:
+1 - x, x+1 - y, y+1 - n
+
+calculate the prefix sum
+prefix sum of part1: Give 1 as +1 & 2,3 as -1 weight. After that traverse the complete array with n+1 size
+prefix sum of part2: Give 3 as -1 and 1,2 as +1 weight. (Do the same)
+
+pref1[x] >= 0, pref2[y] - pref2[x] >= 0  [According to the condition]
+We need to choose the x & y along with the satisfying condn
+
+Take the mn = Infinity; check whether the pref1[i] >= 0 if yes then for the same index compare min(mn,pref2[i]) because we need pef2[y] - pref2[x] so to get pref2[x] we are checking pref1 value and inserting that inside pref2 to get that pref2[x]
+
+Next we check if(pref2[y] - pref2[x] >= 0) we return yes
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
